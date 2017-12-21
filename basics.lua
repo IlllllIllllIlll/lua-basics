59ee35d322fa55b9ba5113505d16ccf46b3a2930
IlllllIllllIlll <i1l5o1l4i3@gmail.com>
2017-12-21 09:32:45 +0000

    some additions before functions

--- basics.lua
+++ basics.lua
@@ -2,8 +2,15 @@
 
 -- Default values
 name=""
-have_key=false;
-i = 0;
+ynPrompt="x"
+have_key=false
+i = 0
+nameBool = false
+selConfirmed = false
+
+
+-- Inventory of wares for the vagrant
+inv = {"book", "sword",  "pen",  "armor"}
 
 -- Start here:
 print("What is your name?")
@@ -13,6 +20,7 @@ name=io.read() -- input
 while (name ~= "Gucci"  and i <4) do 
   print("Hello " .. name .. ", I don't recognize you. Have you another name?")
   if (i<4) then
+	-- Counter to measure co-operativeness
 	name=io.read()
 	i = i+1
   end
@@ -20,18 +28,50 @@ end
 
 -- Once expected input is entered, proceed to next while loop
 while (name=="Gucci" and have_key==false) do
-  print("Hello " .. name .. ", we've been expecting you, have you a password")
-  pass=io.read()
-  if pass=="plies123" then
-    have_key = true
-  end
+	nameBool = true; -- We've confirmed their name, no need for evals
+	print("Hello " .. name .. " we've been expecting you, have you a password")
+	pass=io.read()
+	if pass=="plies123" then
+		have_key = true -- We've confirmed they're keyed
+	end
 end 
 
--- Only gets here if all previous conditions are met. Redundant if statement here, but that's alright.
-if (have_key and name == "Gucci") then
-  print("Thank you " .. name .. " for entering the password " .. pass)
+while (selConfirmed ~= true) do
+	print("Selection made.")
+	-- Only getting your name right in the first instance will
+	-- grant you this menu
+
+	if (have_key and nameBool and i==0) then 
+		while(ynPrompt ~= "y") do
+			--  Keep prompting until yes.
+			print("Pick a number between 1-4")
+			selection=io.read()
+			print("You've picked " .. inv[tonumber(selection)] 
+			.. ". y/n")
+			ynPrompt=io.read()
+		end
+	end
+	if ((i>3 or have_key)  )  then
+		break
+	end
 end
 
-if (i>3) then 
-	print ("You may not proceed.")
+-- Only gets here if all previous conditions are met.
+-- Discriminates based on co-operation level (of name).
+if (have_key and nameBool and i>0) then
+  	print("Thank you " .. name .. " for entering the password " 
+	.. pass .. ". Take also this " .. inv[tonumber(i)] .. ".")
+elseif(have_key and nameBool and i==0) then   
+  	print("You've been very co-operative " .. name .. ", password " 
+	.. pass .. ". You've chosen to take with you this " 
+	.. inv[tonumber(selection)])
+else
+	print ("You may not proceed. Leave here. Take with you this " .. 
+	inv[i] .. ".")
+
 end
+
+
+
+
+
